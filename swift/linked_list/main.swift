e301875aaa7c612a4a16225d1f0993b97a72b15f
Nick Rogers <mnicholasrogers@gmail.com>
2019-07-27 14:44:08 -0700

    Created basic driver program.

--- swift/linked_list/main.swift
+++ swift/linked_list/main.swift
@@ -1,4 +1,26 @@
 import Foundation
 
-print("Hello, World!")
+class Node<T>: Linkable
+{
+    var previous: Linkable?
+    var next: Linkable?
+    
+    var data: T?
+    
+    init(data: T)
+    {
+        self.data = data
+    }
+}
 
+var n1 = Node(data: 1)
+var n2 = Node(data: 2)
+var n3 = Node(data: 3)
+
+let linkedList = LinkedList<Node<Int>>()
+
+linkedList.push(node: &n1)
+linkedList.push(node: &n2)
+linkedList.push(node: &n3)
+
+linkedList.walk()
